e40b9ced4b0b4ef07136554a5f8ce8f4151eb0e4
taewooyun <taewooyunn@gmail.com>
2023-05-16 22:34:35 +0900

    학습 2회차
    - Controller, Service, Domain, Repository 관계
    - 도메인, 레포지토리, 서비스 생성
    - 테스트 케이스 작성

---
+++ tutorial/src/test/java/io/taewooyunn/tutorial/service/MemberServiceTest.java
@@ -0,0 +1,67 @@
+package io.taewooyunn.tutorial.service;
+
+import io.taewooyunn.tutorial.domain.Member;
+import io.taewooyunn.tutorial.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    public void d_m_e(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member1.setName("spring");
+
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        //then
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
